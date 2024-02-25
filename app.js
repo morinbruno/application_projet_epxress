c876e21958f8dd45da1b540fb231d09a1251a76e
Morino_Belgotaku <morinbruno@hotmail.be>
2024-02-25 06:07:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const session = require('express-session');
 const mysql = require('mysql2');
 const app = express();
 const bodyParser = require('body-parser');
-const { Toast } = require('@popperjs/core');
 
 // Paramètres
 const db_connect = require('./settings/db_connection.json');
@@ -56,9 +55,9 @@ function date_local_input(date) {
 }
 
 function date_peremption_etat(date) {
-	if (date_local(new Date) == date) {
+	if (date_local(new Date) == date_local(date)) {
 		return "Dernier jour de consommation"
-	} else if (date_local(new Date) < date) {
+	} else if (date_local(new Date) < date_local(date)) {
 		return "Comestible"
 	} else {
 		return "Non comestible"
@@ -72,14 +71,23 @@ function date_peremption_etat(date) {
  */
 
 app.get(`/dashboard`, function (req, res) {
-	let sql = `SELECT * FROM users JOIN produits_acheter 
+	let sql = `SELECT produits.nom_produit, produits_acheter.quantite, 
+	magasins.nom_magasin, produits_acheter.date_achat, produits_acheter.date_expiration, produits.id_produit, 
+	localite.nom_localite, categories.nom_categorie FROM users JOIN produits_acheter 
 	ON users.id_user=produits_acheter.id_user JOIN produits 
 	ON produits_Acheter.id_produit=produits.id_produit JOIN magasins_produits 
 	ON produits.id_produit=magasins_produits.id_produit JOIN magasins 
 	ON magasins_produits.id_magasin=magasins.id_magasin JOIN localite 
 	ON magasins_produits.code_postal=localite.code_postal JOIN categories
 	ON produits.code_categorie=categories.code_categorie
-	WHERE produits_acheter.id_user = ?`;
+	WHERE produits_acheter.id_user = ? ORDER BY produits.nom_produit;
+
+	SELECT * FROM categories ORDER BY nom_categorie;
+
+	SELECT * FROM magasins ORDER BY nom_magasin;
+
+	SELECT * FROM localite ORDER BY nom_localite;
+	`;
 
 	connection.query(sql, [req.session.id_user], function (erreur, resultat) {
 		if (req.session.loggedin) {
@@ -90,14 +98,16 @@ app.get(`/dashboard`, function (req, res) {
 				date_local,
 				date_local_input,
 				date_peremption_etat,
-				Toast,
+				list_produit: resultat[0],
+				list_categorie: resultat[1],
+				list_magasin: resultat[2],
+				list_localite: resultat[3],
 				req
 			});
 		} else {
 			res.redirect('/');
 		}
-		res.end();
-	});
+	});	
 });
 
 
