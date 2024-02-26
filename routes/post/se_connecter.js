f6abcbc796daeb68b5a1b2c634c13397652b706f
Morino_Belgotaku <morinbruno@hotmail.be>
2024-02-26 17:43:58 +0100

    Update se_connecter.js

--- routes/post/se_connecter.js
+++ routes/post/se_connecter.js
@@ -36,7 +36,7 @@ router.post('/se-connecter', function (req, res) {
 				req.session.username = resultat[0]['user'];
 				req.session.typeuser = resultat[0]['name_typeUser'];
 				req.session.id_user = resultat[0]['id_user'];
-				req.session.userinfo = resultat;
+				req.session.user_info = resultat[0];
 				res.redirect('/dashboard');
 			} else {
 				res.redirect('/?invalid_user=true');
