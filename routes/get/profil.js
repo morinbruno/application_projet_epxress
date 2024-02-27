6ef16b9a0d4f0a571b1f0374135f4871658eb910
Morino_Belgotaku <morinbruno@hotmail.be>
2024-02-27 03:25:59 +0100

    Update profil.js

--- routes/get/profil.js
+++ routes/get/profil.js
@@ -3,11 +3,22 @@ const router = express.Router();
 const nav = require('../../settings/nav_bar.json');
 
 router.get('/profil', function (req, res) {
+	let password_invalid = req.query.password_invalid;
+	let password_different = req.query.password_different;
+	let password_short = req.query.password_short;
+	let password_long = req.query.password_long;
+	let password_update_success = req.query.password_update_success;
+
 	if (req.session.loggedin) {
 		res.render('pages/profil', {
 			title: "Profil",
 			nav,
-			req
+			req,
+			password_invalid,
+			password_different,
+			password_short,
+			password_long,
+			password_update_success
 		});
 	}
 	else {
