7bce1e6abdccd8083d3d48c0e952c9accfc31d39
Morino_Belgotaku <morinbruno@hotmail.be>
2024-03-01 09:19:29 +0100

    Update admin.js

--- routes/get/admin.js
+++ routes/get/admin.js
@@ -15,17 +15,47 @@ const connection = mysql.createConnection({
 
 router.get('/admin', function (req, res) {
 	if (req.session.typeuser === "Admin") {
+		let password_update_success = req.query.password_update_success;
+
 		let sql = `SELECT * FROM users JOIN typeuser 
 		           ON users.typeAccount=typeuser.id_typeUser 
 				   WHERE typeuser.name_typeUser='Utilisateur'`
 
-		connection.query(sql,function(erreur, resultat) {
+		connection.query(sql, function (erreur, resultat) {
 			let list_users = resultat;
+			let id_user = req.query.id_user;
+			let password_different = req.query.password_different;
+			let password_short = req.query.password_short;
+			let password_long = req.query.password_long;
+			let password_invalid = req.query.password_invalid;
+
+			let pseudo_exist = req.query.pseudo_exist;
+			let email_exist = req.query.email_exist;
+			let pseudo_invalid = req.query.pseudo_invalid;
+			let mdp_invalid = req.query.mdp_invalid;
+			let mdp_short = req.query.mdp_short;
+			let mdp_long = req.query.mdp_long;
+			let pseudo_short = req.query.pseudo_short;
+			let pseudo_long = req.query.pseudo_long;
 
 			res.render('pages/admin', {
 				title: "Admin",
 				nav,
 				list_users,
+				password_update_success,
+				id_user,
+				password_different,
+				password_short,
+				password_long,
+				password_invalid,
+				pseudo_exist,
+				email_exist,
+				pseudo_invalid,
+				mdp_invalid,
+				mdp_short,
+				mdp_long,
+				pseudo_short,
+				pseudo_long,
 				req
 			})
 		})
