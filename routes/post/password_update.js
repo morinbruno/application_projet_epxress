f1b9108e3822f9c8c94958a56d1ee66b62c74f8f
Morino_Belgotaku <morinbruno@hotmail.be>
2024-03-01 09:20:07 +0100

    Add admin modify user password conditions

--- routes/post/password_update.js
+++ routes/post/password_update.js
@@ -15,23 +15,43 @@ const connection = mysql.createConnection({
 router.post('/password-update', function (req, res) {
 	let password = req.body.password;
 	let password_repeat = req.body.password_repeat;
+	let id_user = req.body.id_user;
 
-	if(password.includes(' ') || password.length == 0 || password_repeat.length == 0) {
-		res.redirect('/profil?password_invalid=true')
+	if (id_user && req.session.typeuser == "Admin") {
+		if (password.includes(' ') || password.length == 0 || password_repeat.length == 0) {
+			res.redirect(`/admin?password_invalid=true&id_user=${id_user}`)
 
-	} else if (password.length < 3) {
-		res.redirect('/profil?password_short=true')
-	} else if(password.length > 20) {
-		res.redirect('/profil?password_long=true')
-	} else if (password != password_repeat) {
-		res.redirect('/profil?password_different=true')
+		} else if (password.length < 3) {
+			res.redirect(`/admin?password_short=true&id_user=${id_user}`)
+		} else if (password.length > 20) {
+			res.redirect(`/admin?password_long=true&id_user=${id_user}`)
+		} else if (password != password_repeat) {
+			res.redirect(`/admin?password_different=true&id_user=${id_user}`)
+		} else {
+			let sql = `UPDATE users SET password= ? WHERE id_user= ?`
+
+			connection.query(sql, [password, id_user], function (erreur, resultat) {
+				res.redirect(`/admin?password_update_success=true&id_user=${id_user}`)
+			})
+		}
 	} else {
-		let sql = `UPDATE users SET password= ? WHERE id_user= ?`
-		let id_user = req.session.id_user;
+		if (password.includes(' ') || password.length == 0 || password_repeat.length == 0) {
+			res.redirect('/profil?password_invalid=true')
 
-		connection.query(sql, [password, id_user], function(erreur, resultat) {
-			res.redirect('/profil?password_update_success=true')
-		})
+		} else if (password.length < 3) {
+			res.redirect('/profil?password_short=true')
+		} else if (password.length > 20) {
+			res.redirect('/profil?password_long=true')
+		} else if (password != password_repeat) {
+			res.redirect('/profil?password_different=true')
+		} else {
+			let sql = `UPDATE users SET password= ? WHERE id_user= ?`
+			let id_user = req.session.id_user;
+
+			connection.query(sql, [password, id_user], function (erreur, resultat) {
+				res.redirect('/profil?password_update_success=true')
+			})
+		}
 	}
 });
 
