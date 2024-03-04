9fbd9f494166ee896001af7e8fe0809bcfd7fc90
Morino_Belgotaku <morinbruno@hotmail.be>
2024-03-04 15:27:18 +0100

    Create ajouter_element.js

---
+++ routes/ajouter_element.js
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+const router = Router();
+import { createConnection } from 'mysql2';
+import db_connection from '../settings/db_connection.json' with { type: "json" };
+
+// Création d'une connexion à la base de données
+const connection = createConnection({
+	host: db_connection.host,
+	user: db_connection.user,
+	password: db_connection.password,
+	database: db_connection.database,
+	multipleStatements: true
+});
+
+router.post('/ajouter-element', function (req, res) {
+    const ajout_localite = req.body.ajout_localite;
+    const ajout_code_postal = req.body.ajout_code_postal;
+
+    if(ajout_localite && ajout_code_postal) {
+        let sql = `INSERT INTO localite VALUES(?,?);`
+
+        connection.query(sql, [ajout_code_postal,ajout_localite], function(erreur, resultat){
+            res.redirect('/admin?ajout_localite=success')
+        })
+    } else {
+        res.redirect('/admin?ajout_localite=erreur')
+    }
+});
+
+export default router;
