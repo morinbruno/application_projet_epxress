a47dce830c79ca3c5e7c0abc8c58c1b90d7b128a
Morino_Belgotaku <morinbruno@hotmail.be>
2024-03-04 17:02:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
 
 
