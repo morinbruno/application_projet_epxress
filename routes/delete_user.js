d7a30ec600b4c65fbfc4ec2d556b7e345e288bc4
Morino_Belgotaku <morinbruno@hotmail.be>
2024-03-07 00:45:18 +0100

    Update delete_user.js

--- routes/delete_user.js
+++ routes/delete_user.js
@@ -14,40 +14,46 @@ const connection = mysql.createConnection({
 
 router.post('/delete-user', function (req, res) {
 	let id_user = req.body.id_user;
-    let sql = `SELECT produits_acheter.id_produit FROM produits_acheter JOIN users ON produits_acheter.id_user=users.id_user WHERE produits_acheter.id_user= ?`
+    
+    if (id_user == req.session.id_user || req.session.typeuser == 'Admin') {
+        let sql = `SELECT produits_acheter.id_produit FROM produits_acheter JOIN users ON produits_acheter.id_user=users.id_user WHERE produits_acheter.id_user= ?`
 
-    connection.query(sql, [id_user, id_user], function(erreur, resultat) {
-        let id_produit_user = [];
-        let id_user = req.body.id_user;
-        let sql = null;
-
-        for (let i=0; i < resultat.length; i++) {
-            id_produit_user.push(resultat[i]['id_produit']);
-        }
-        id_produit_user = id_produit_user.toString();
-
-        if (id_produit_user.length == 0) {
-            sql = `DELETE FROM users WHERE id_user= ?;`
-        } else {
-            sql = `DELETE FROM magasins_produits WHERE id_produit IN (${id_produit_user});
-            DELETE FROM produits_acheter WHERE id_produit IN (${id_produit_user});
-            DELETE FROM produits WHERE id_produit IN (${id_produit_user});
-            DELETE FROM users WHERE id_user= ?;`
-        }
-
-        connection.query(sql, [id_user], function(erreur, resultat) {
-            if(req.session.lastRoute != '/admin') {
-                req.session.loggedin = false;
-                req.session.username = null;
-				req.session.typeuser = null;
-				req.session.id_user = null;
-				req.session.user_info = null;
-                res.redirect('/')
-            } else {
-                res.redirect('/admin')
+        connection.query(sql, [id_user, id_user], function(erreur, resultat) {
+            let id_produit_user = [];
+            let id_user = req.body.id_user;
+            let sql = null;
+    
+            
+            for (let i=0; i < resultat.length; i++) {
+                id_produit_user.push(resultat[i]['id_produit']);
             }
+            id_produit_user = id_produit_user.toString();
+    
+            if (id_produit_user.length == 0) {
+                sql = `DELETE FROM users WHERE id_user= ?;`
+            } else {
+                sql = `DELETE FROM magasins_produits WHERE id_produit IN (${id_produit_user});
+                DELETE FROM produits_acheter WHERE id_produit IN (${id_produit_user});
+                DELETE FROM produits WHERE id_produit IN (${id_produit_user});
+                DELETE FROM users WHERE id_user= ?;`
+            }
+    
+            connection.query(sql, [id_user], function(erreur, resultat) {
+                if(req.session.lastRoute != '/admin') {
+                    req.session.loggedin = false;
+                    req.session.username = null;
+                    req.session.typeuser = null;
+                    req.session.id_user = null;
+                    req.session.user_info = null;
+                    res.redirect('/')
+                } else {
+                    res.redirect('/admin')
+                }
+            })
         })
-    })
+    } else {
+        res.redirect(req.session.lastRoute)
+    }
 });
 
 module.exports = router;
